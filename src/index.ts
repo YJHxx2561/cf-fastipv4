21fd086f3a5dda41d2b9a9124e39346167c34413
YJHxx2561 <167593692+YJHxx2561@users.noreply.github.com>
2026-02-06 04:07:00 +0800

    更新 index.ts

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ async function handleCron(env: Env) {
     }
 
     const sourceDomain = 'cf.090227.xyz';
-    const targetDomain = 'cf.hw.072103.xyz';
+    const targetDomain = 'goodcf.hw.024686.xyz';
 
     // 1. Resolve IPs
     console.log(`Resolving IPs for ${sourceDomain}...`);
